541e63af4d06b1e3a270378042a0978a6d4d56ea
Anubhav Jain <ajain@lbl.gov>
2013-04-11 00:16:36 -0700

    fix silly typo

--- rubicon/firetasks/gaussian_task.py
+++ rubicon/firetasks/gaussian_task.py
@@ -20,7 +20,7 @@ class GaussianTask(FireTaskBase, FWSerializable):
     _fw_name = "Gaussian Task"
 
     def run_task(self, fw_spec):
-        mol = Molecule.from_dict(fw_spec['mol'])
+        mol = Molecule.from_dict(fw_spec['molecule'])
         gi = GaussianInput(mol, fw_spec['charge'], fw_spec['spin_multiplicity'], fw_spec['title'], fw_spec['functional'], fw_spec['basis_set'], fw_spec['route_parameters'], fw_spec['input_parameters'], fw_spec['link0_parameters'])
 
         with open('gaussian.in') as f:
