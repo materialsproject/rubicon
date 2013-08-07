f2b60613d5733dadb3a4bb00f2ff64090bc9f035
Xiaohui Qu <xqu@lbl.gov>
2013-08-07 14:52:29 -0700

    nwchem test

---
+++ scripts/nwchem_test.py
@@ -0,0 +1,20 @@
+from pymatgen import Molecule
+from pymatgen.io.nwchemio import NwInput, NwTask
+
+coords = [[0.000000, 0.000000, 0.000000],
+          [0.000000, 0.000000, 1.089000],
+          [1.026719, 0.000000, -0.363000],
+          [-0.513360, -0.889165, -0.363000],
+          [-0.513360, 0.889165, -0.363000]]
+mol = Molecule(["C", "H", "H", "H", "H"], coords)
+
+tasks = [
+            NwTask.dft_task(mol, operation="optimize", xc="b3lyp",
+                            basis_set="6-31++G*"),
+            NwTask.dft_task(mol, operation="freq", xc="b3lyp",
+                            basis_set="6-31++G*")
+
+        ]
+nwi = NwInput(mol, tasks)
+
+print str(nwi)
