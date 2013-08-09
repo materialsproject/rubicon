e281a1bebcc6a9df232869a3eb340e54ca09b115
Xiaohui Qu <xqu@lbl.gov>
2013-08-09 11:26:59 -0700

    remove pull test for nidhi t 2

--- rubicon/workflows/mol_to_wf.py
+++ rubicon/workflows/mol_to_wf.py
@@ -5,8 +5,6 @@ from pymatgen.io.xyzio import XYZ
 from rubicon.firetasks.gaussian_task import GaussianTask
 from rubicon.firetasks.nwchem_task import NWChemTask
 
-# pull test for nidhi .. v 2.
-
 __author__ = 'Anubhav Jain'
 __copyright__ = 'Copyright 2013, The Materials Project'
 __version__ = '0.1'
