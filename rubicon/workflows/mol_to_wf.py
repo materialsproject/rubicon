915d8c1f546648a973bef0e5939d60a073d4fffb
Xiaohui Qu <xqu@lbl.gov>
2013-09-03 13:40:24 -0700

    remove ODFT option, use RHF for molecular species

--- rubicon/workflows/mol_to_wf.py
+++ rubicon/workflows/mol_to_wf.py
@@ -64,19 +64,19 @@ def mol_to_wf_nwchem(mol, name):
 
     tasks = [
         NwTask.dft_task(mol, operation="optimize", xc="b3lyp",
-                        basis_set="6-31++G*", theory_directives={"odft": "", "iterations": 300})]
+                        basis_set="6-31++G*", theory_directives={"iterations": 300})]
 
     if len(mol.sites) > 1:
         tasks += [NwTask.dft_task(mol, operation="freq", xc="b3lyp",
-                        basis_set="6-31++G*", theory_directives={"odft": "", "iterations": 300})]
+                        basis_set="6-31++G*", theory_directives={"iterations": 300})]
 
     tasks += [
         NwTask.dft_task(mol, operation="energy", xc="b3lyp",
-                        basis_set="6-311++G**", theory_directives={"odft": "", "iterations": 300}),
+                        basis_set="6-311++G**", theory_directives={"iterations": 300}),
         NwTask.dft_task(mol, charge=mol.charge + 1, operation="energy",
-                        xc="b3lyp", basis_set="6-311++G**", theory_directives={"odft": "", "iterations": 300}),
+                        xc="b3lyp", basis_set="6-311++G**", theory_directives={"iterations": 300}),
         NwTask.dft_task(mol, charge=mol.charge - 1, operation="energy",
-                        xc="b3lyp", basis_set="6-311++G**", theory_directives={"odft": "", "iterations": 300})
+                        xc="b3lyp", basis_set="6-311++G**", theory_directives={"iterations": 300})
     ]
 
     nwi = NwInput(mol, tasks)
