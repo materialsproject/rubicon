767b92482be6c86b31b7255c74bced5a98b53952
Xiaohui Qu <xqu@lbl.gov>
2013-09-09 19:47:57 -0700

    also include molecules without experimental data

--- rubicon/testset/get_g3_benchmark.py
+++ rubicon/testset/get_g3_benchmark.py
@@ -31,7 +31,7 @@ def get_g3_bench_collection():
 
 
 
-def get_calcualtion_result(mission_tag, bench_key_name, bench_dict, db_collection):
+def get_calcualtion_result(mission_tag, bench_key_name, bench_dict, no_expt_bench_dict, db_collection):
     result_cursor = db_collection.find({"user_tags.mission": mission_tag},
                              fields=['pretty_formula', 'IE', 'EA', 'charge',
                                      'user_tags.fw_name'])
@@ -45,21 +45,41 @@ def get_calcualtion_result(mission_tag, bench_key_name, bench_dict, db_collectio
         if fw_name in gau2web_name_map:
             web_name = gau2web_name_map[fw_name]
             d = bench_dict[web_name]
-            if 'IE' in m:
-                if 'IP' in d:
-                    d['IP'][bench_key_name] = m['IE']
-                else:
-                    d['IP'] = {bench_key_name: m['IE']}
-            if 'EA' in m:
-                if 'EA' in d:
-                    d['EA'][bench_key_name] = m['EA']
-                else:
-                    d['EA'] = {bench_key_name: m['EA']}
+        else:
+            if not fw_name in no_expt_bench_dict:
+                no_expt_bench_dict[fw_name] = {}
+            d = no_expt_bench_dict[fw_name]
 
-def write_csv(bench):
+        if 'IE' in m:
+            if 'IP' in d:
+                d['IP'][bench_key_name] = m['IE']
+            else:
+                d['IP'] = {bench_key_name: m['IE']}
+        if 'EA' in m:
+            if 'EA' in d:
+                d['EA'][bench_key_name] = m['EA']
+            else:
+                d['EA'] = {bench_key_name: m['EA']}
+
+
+def write_dict(bench_dict, writer):
+    mols = sorted(bench_dict.keys())
+    if 'unit' in mols:
+        mols.remove('unit')
+    for m in mols:
+        row_dict = defaultdict(str)
+        row_dict["Molecule"] = m
+        for property, source_names in bench_dict[m].items():
+            for source in source_names:
+                col_name = '{}-{}'.format(property, source)
+                row_dict[col_name] = bench_dict[m][property][source]
+        writer.writerow(row_dict)
+
+
+def write_csv(bench_dict, no_expt_bench_dict):
     source_names = set()
     property_names = set()
-    for m in bench.items():
+    for m in bench_dict.items():
         if m[0] != 'unit':
             for i, v in m[1].items():
                 property_names.add(i)
@@ -70,16 +90,8 @@ def write_csv(bench):
     with open("G3_bench.csv", 'w') as f:
         writer = csv.DictWriter(f, fieldnames=headings)
         writer.writeheader()
-        mols = sorted(bench.keys())
-        mols.remove('unit')
-        for m in mols:
-            row_dict = defaultdict(str)
-            row_dict["Molecule"] = m
-            for property, source_names in bench[m].items():
-                for source in source_names:
-                    col_name = '{}-{}'.format(property, source)
-                    row_dict[col_name] = bench[m][property][source]
-            writer.writerow(row_dict)
+        write_dict(bench_dict, writer)
+        write_dict(no_expt_bench_dict, writer)
 
 
 if __name__ == '__main__':
@@ -92,13 +104,17 @@ if __name__ == '__main__':
             for i, v in m[1].items():
                 v['Expt'] *= KCAL_TO_EV
                 v['G3'] *= KCAL_TO_EV
+    no_expt_bench = {}
 
     collection = get_g3_bench_collection()
 
-    get_calcualtion_result("G2-97 Test Set Benchmark (Shyue Scheme)", "Shyue", bench, collection)
-    get_calcualtion_result("G2-97 Test Set Benchmark (Larry Scheme)", "Larry", bench, collection)
+    get_calcualtion_result("G2-97 Test Set Benchmark (Shyue Scheme)", "Shyue", bench, no_expt_bench, collection)
+    get_calcualtion_result("G2-97 Test Set Benchmark (Larry Scheme)", "Larry", bench, no_expt_bench, collection)
 
+    all_bench = {}
+    all_bench.update(bench)
+    all_bench.update(no_expt_bench)
     with open("G3_bench.json", 'w') as f:
-        json.dump(bench, f, indent=4, sort_keys=True)
+        json.dump(all_bench, f, indent=4, sort_keys=True)
 
-    write_csv(bench)
+    write_csv(bench, no_expt_bench)
