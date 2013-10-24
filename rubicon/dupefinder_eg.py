4f346611eacce9438f3d9147ebfa9b42ee3d4aac
Xiaohui Qu <xqu@lbl.gov>
2013-10-23 17:42:18 -0700

    add class DupeFinderEG

---
+++ rubicon/dupefinder_eg.py
@@ -0,0 +1,19 @@
+from fireworks.features.dupefinder import DupeFinderBase
+
+__author__ = 'xiaohuiqu'
+
+
+class DupeFinderEG(DupeFinderBase):
+    """
+    General Duplication finder for Electrolyte Genome project
+    """
+
+    _fw_name = 'Dupe Finder EG'
+
+    def verify(self, spec1, spec2):
+        # assert: task_type and snlgroup_id have already been checked through query
+        return set(spec1['user_tags']) == set(spec2['user_tags'])
+
+    def query(self, spec):
+        return {'spec.task_type': spec['task_type'],
+                'spec.snlgroup_id': spec['snlgroup_id']}
