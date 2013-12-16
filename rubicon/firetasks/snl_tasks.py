c4be599ba9cb5d0f3f0de3d6bd0bb6d91dff3caf
Xiaohui Qu <xqu@lbl.gov>
2013-12-16 15:35:22 -0800

    reforme AddSNLTask

--- rubicon/firetasks/snl_tasks.py
+++ rubicon/firetasks/snl_tasks.py
@@ -12,14 +12,17 @@ class AddSNLTask(FireTaskBase, FWSerializable):
     _fw_name = "Add SNL Task"
 
     def run_task(self, fw_spec):
-        # pass-through option for when we start with an mpsnl and don't actually want to add
+        # pass-through option for when we start with an egsnl and don't
+        # actually want to add
         if 'force_egsnl' in fw_spec and 'force_snlgroup_id' in fw_spec:
             print 'USING FORCED EGSNL'
             return FWAction(update_spec={'egsnl': fw_spec['force_egsnl'],
-                                         'snlgroup_id': fw_spec['force_snlgroup_id']})
+                                         'snlgroup_id':
+                                         fw_spec['force_snlgroup_id']})
 
         sma = SNLMongoAdapter.auto_load()
         snl = StructureNL.from_dict(fw_spec['snl'])
         egsnl, snlgroup_id = sma.add_snl(snl)
 
-        return FWAction(update_spec={'egsnl': egsnl.to_dict, 'snlgroup_id': snlgroup_id})
+        return FWAction(update_spec={'egsnl': egsnl.to_dict,
+                                     'snlgroup_id': snlgroup_id})
