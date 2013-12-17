e9ea7cc04d27cc6c5d8e69924ef23043fc43d46a
Xiaohui Qu <xqu@lbl.gov>
2013-12-16 17:04:50 -0800

    reformat process submissions

--- rubicon/processors/process_submissions.py
+++ rubicon/processors/process_submissions.py
@@ -33,26 +33,33 @@ class SubmissionProcessor():
 
     def submit_new_workflow(self):
         # finds a submitted job, creates a workflow, and submits it to FireWorks
-        job = self.jobs.find_and_modify({'state': 'SUBMITTED'}, {'$set': {'state': 'WAITING'}})
+        job = self.jobs.find_and_modify({'state': 'SUBMITTED'},
+                                        {'$set': {'state': 'WAITING'}})
         if job:
             submission_id = job['submission_id']
+            # noinspection PyBroadException
             try:
                 if 'snl_id' in job:
                     snl = EGStructureNL.from_dict(job)
                 else:
                     snl = StructureNL.from_dict(job)
                 if len(snl.structure.sites) > SubmissionProcessor.MAX_SITES:
-                    self.sma.update_state(submission_id, 'REJECTED', 'too many sites', {})
-                    print 'REJECTED WORKFLOW FOR {} - too many sites ({})'.format(
-                        snl.structure.formula, len(snl.structure.sites))
+                    self.sma.update_state(submission_id, 'REJECTED',
+                                          'too many sites', {})
+                    print 'REJECTED WORKFLOW FOR {} - too many sites ' \
+                          '({})'.format(snl.structure.formula,
+                                        len(snl.structure.sites))
                 elif not job['is_valid']:
                     self.sma.update_state(submission_id, 'REJECTED',
-                                          'invalid structure (atoms too close)', {})
-                    print 'REJECTED WORKFLOW FOR {} - invalid structure'.format(
-                        snl.structure.formula)
+                                          'invalid structure (atoms too close)',
+                                          {})
+                    print 'REJECTED WORKFLOW FOR {} - invalid ' \
+                          'structure'.format(snl.structure.formula)
                 else:
-                    snl.data['_electrolytegenome'] = snl.data.get('_electrolytegenome', {})
-                    snl.data['_electrolytegenome']['submission_id'] = submission_id
+                    snl.data['_electrolytegenome'] = \
+                        snl.data.get('_electrolytegenome', {})
+                    snl.data['_electrolytegenome']['submission_id'] \
+                        = submission_id
 
                     # create a workflow
                     wf = snl_to_wf(snl, job['parameters'])
@@ -66,13 +73,19 @@ class SubmissionProcessor():
             return submission_id
 
     def update_existing_workflows(self):
-        # updates the state of existing workflows by querying the FireWorks database
-        for submission in self.jobs.find({'state': {'$nin': ['COMPLETED', 'ERROR', 'REJECTED']}},
+        # updates the state of existing workflows by querying the FireWorks
+        # database
+        for submission in self.jobs.find({'state': {'$nin': ['COMPLETED',
+                                                             'ERROR',
+                                                             'REJECTED']}},
                                          {'submission_id': 1}):
             submission_id = submission['submission_id']
+            # noinspection PyBroadException
             try:
                 # get a wf with this submission id
-                fw_id = self.launchpad.get_wf_ids({'metadata.submission_id': submission_id}, limit=1)[0]
+                fw_id = self.launchpad.get_wf_ids({'metadata.submission_id':
+                                                   submission_id},
+                                                  limit=1)[0]
                 # get a workflow
                 wf = self.launchpad.get_wf_by_fw_id(fw_id)
                 # update workflow
@@ -99,12 +112,14 @@ class SubmissionProcessor():
                             machine_name = 'mendel/carver'
                         break
                 if fw.state == 'RESERVED':
-                    details = 'queued to run: {} on {}'.format(fw.spec['task_type'], machine_name)
+                    details = 'queued to run: {} on {}'.format(
+                        fw.spec['task_type'], machine_name)
                 if fw.state == 'RUNNING':
-                    details = 'running: {} on {}'.format(fw.spec['task_type'], machine_name)
+                    details = 'running: {} on {}'.format(
+                        fw.spec['task_type'], machine_name)
                 if fw.state == 'FIZZLED':
-                    details = 'fizzled while running: {} on {}'.format(fw.spec['task_type'],
-                                                                       machine_name)
+                    details = 'fizzled while running: {} on {}'.format(
+                        fw.spec['task_type'], machine_name)
 
         m_taskdict = {}
         states = [fw.state for fw in wf.fws]
@@ -113,7 +128,8 @@ class SubmissionProcessor():
                 if fw.state == 'COMPLETED':
                     for l in fw.launches:
                         # if task_id is not there, it means we went on DETOUR...
-                        if l.state == 'COMPLETED' and 'task_id' in l.action.stored_data:
+                        if l.state == 'COMPLETED' and \
+                                      'task_id' in l.action.stored_data:
                             t_id = l.action.stored_data['task_id']
                             if 'task_type' in fw.spec:
                                 m_taskdict[fw.spec['task_type']] = t_id
