8ccb7dcb2b2f979dada901203444df792c6ce1fc
Xiaohui Qu <xqu@lbl.gov>
2013-12-16 17:17:24 -0800

    reformat submissions_run

--- rubicon/scripts/submissions_run.py
+++ rubicon/scripts/submissions_run.py
@@ -3,14 +3,17 @@ from rubicon.processors.process_submissions import SubmissionProcessor
 
 
 def go_submissions():
-    m_description = 'This program is used to pull jobs from the Submissions database, ' \
-                    'create FireWorks workflows from those submissions, and then monitor ' \
-                    'all previous submissions for updates to state (so that the submission ' \
+    m_description = 'This program is used to pull jobs from the Submissions ' \
+                    'database, create FireWorks workflows from those ' \
+                    'submissions, and then monitor all previous submissions ' \
+                    'for updates to state (so that the submission ' \
                     'database can be updated)'
 
     parser = ArgumentParser(description=m_description)
-    parser.add_argument('--sleep', help='sleep time between loops', default=None, type=int)
-    parser.add_argument('--infinite', help='loop infinite times', action='store_true')
+    parser.add_argument('--sleep', help='sleep time between loops',
+                        default=None, type=int)
+    parser.add_argument('--infinite', help='loop infinite times',
+                        action='store_true')
     args = parser.parse_args()
 
     sp = SubmissionProcessor.auto_load()
