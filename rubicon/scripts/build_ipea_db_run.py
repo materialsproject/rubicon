e16dd60a32be5fe41000de62234322b511026404
Xiaohui Qu <xqu@lbl.gov>
2013-12-16 17:15:54 -0800

    reformat build ipea db

--- rubicon/scripts/build_ipea_db_run.py
+++ rubicon/scripts/build_ipea_db_run.py
@@ -5,9 +5,10 @@ from rubicon.submission.submission_mongo import SubmissionMongoAdapter
 
 __author__ = 'xiaohuiqu'
 
+
 def get_calculation_db():
     db_dir = os.environ['DB_LOC']
-    db_path = os.path.join(db_dir, 'nwchem_calc_db.json')
+    db_path = os.path.join(db_dir, 'qchem_calc_db.json')
     with open(db_path) as f:
         db_creds = json.load(f)
     conn = MongoClient(db_creds['host'], db_creds['port'])
@@ -17,6 +18,7 @@ def get_calculation_db():
     calc_coll = db[db_creds['collection']]
     ipea_coll = db[db_creds['ipea_collection']]
     return calc_coll, ipea_coll
+
 
 def build_ipea_db():
     sma = SubmissionMongoAdapter.auto_load()
@@ -46,31 +48,37 @@ def build_ipea_db():
             anion_tid = j['task_dict']['anion Single Point Energy']
 
         if neutral_tid:
-            neutral_energy_dict = calc_coll.find({'task_id': neutral_tid},
-                                                    fields={"_id":False,
-                                                            "calculations.scf.energies": True,
-                                                            "calculations.sol_scf.energies": True})[0]
-            neutral_vaccum_energy = neutral_energy_dict['calculations']['scf']['energies'][0]
-            neutral_sol_energy = neutral_energy_dict['calculations']['sol_scf'] \
-                                                    ['energies'][0]['sol phase']
+            neutral_energy_dict = calc_coll.find(
+                {'task_id': neutral_tid},
+                fields={"_id": False,
+                        "calculations.scf.energies": True,
+                        "calculations.sol_scf.energies": True})[0]
+            neutral_vaccum_energy = neutral_energy_dict['calculations'][
+                'scf']['energies'][0]
+            neutral_sol_energy = neutral_energy_dict['calculations'][
+                'sol_scf']['energies'][0]['sol phase']
 
         if cation_tid:
-            cation_energy_dict = calc_coll.find({'task_id': cation_tid},
-                                                    fields={"_id":False,
-                                                            "calculations.scf.energies": True,
-                                                            "calculations.sol_scf.energies": True})[0]
-            cation_vaccum_energy = cation_energy_dict['calculations']['scf']['energies'][0]
-            cation_sol_energy = cation_energy_dict['calculations']['sol_scf'] \
-                                                  ['energies'][0]['sol phase']
+            cation_energy_dict = calc_coll.find(
+                {'task_id': cation_tid},
+                fields={"_id": False,
+                        "calculations.scf.energies": True,
+                        "calculations.sol_scf.energies": True})[0]
+            cation_vaccum_energy = cation_energy_dict['calculations']['scf'][
+                'energies'][0]
+            cation_sol_energy = cation_energy_dict['calculations']['sol_scf'][
+                'energies'][0]['sol phase']
 
         if anion_tid:
-            anion_energy_dict = calc_coll.find({'task_id': anion_tid},
-                                                    fields={"_id":False,
-                                                            "calculations.scf.energies": True,
-                                                            "calculations.sol_scf.energies": True})[0]
-            anion_vaccum_energy = anion_energy_dict['calculations']['scf']['energies'][0]
-            anion_sol_energy = anion_energy_dict['calculations']['sol_scf'] \
-                                                  ['energies'][0]['sol phase']
+            anion_energy_dict = calc_coll.find(
+                {'task_id': anion_tid},
+                fields={"_id": False,
+                        "calculations.scf.energies": True,
+                        "calculations.sol_scf.energies": True})[0]
+            anion_vaccum_energy = anion_energy_dict['calculations']['scf'][
+                'energies'][0]
+            anion_sol_energy = anion_energy_dict['calculations']['sol_scf'][
+                'energies'][0]['sol phase']
         if neutral_tid:
             if cation_tid:
                 ip_vaccum = cation_vaccum_energy - neutral_vaccum_energy
@@ -87,8 +95,9 @@ def build_ipea_db():
             if anion_tid:
                 d['ea'] = {'vaccum': ea_vaccum, 'sol': ea_sol}
         if 'ip' in d or 'ea' in d:
-            ipea_coll.update({'submission_id':j['submission_id']}, {"$set": d}, upsert=True)
-
+            ipea_coll.update({'submission_id': j['submission_id']},
+                             {"$set": d},
+                             upsert=True)
 
 
 if __name__ == '__main__':
