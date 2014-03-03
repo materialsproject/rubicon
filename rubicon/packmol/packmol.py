719b796853acb8c44e703d34ed7f06764624368d
Yong Zhang <yzhang19@nd.edu>
2014-03-03 10:36:44 -0800

    calculated box size, no need to input

--- rubicon/packmol/packmol.py
+++ rubicon/packmol/packmol.py
@@ -35,23 +35,36 @@ class PackmolRunner(object):
         else:
             return some_obj
 
-    def _get_auto_boxsize(self, idx):
+    def _get_auto_boxsize(self):
         """
         TODO: Put docs here!!
         :param idx:
         :raise NotImplementedError:
         """
 
-        mol=self.mols[idx]
+        volume=0.0
+        for idx,mol in enumerate(self.mols):
+#        for mol in self.mols
+#        mol=self.mols[idx]
 #        print mol[0]
 #        print self.mols[idx][0]
 #        lx=max(mol[i].coords[0] for i,atom in enumerate(mol))-min(mol[i].coords[0] for i,atom in enumerate(mol))
 #        ly=max(mol[i].coords[1] for i,atom in enumerate(mol))-min(mol[i].coords[1] for i,atom in enumerate(mol))
 #        lz=max(mol[i].coords[2] for i,atom in enumerate(mol))-min(mol[i].coords[2] for i,atom in enumerate(mol))
-        lx,ly,lz= np.max(mol.cart_coords,0)-np.min(mol.cart_coords,0)
-        length=max(lx,ly,lz)+2.0
-        length=length*int(self.param_list[idx]['number'])
-        self.param_list[idx]['inside box']='0. 0. 0. {} {} {}'.format(length,length,length)
+            lx,ly,lz = np.max(mol.cart_coords,0)-np.min(mol.cart_coords,0)
+            print lx,ly,lz
+            lx += 2.0
+            ly += 2.0
+            lz += 2.0
+            length=max(lx,ly,lz)
+            print length
+#            length=length*int(self.param_list[idx]['number'])
+            volume += length**(3.0)*float(self.param_list[idx]['number'])
+        length = volume**(1.0/3.0)
+        print length
+
+        for idx,mol in enumerate(self.mols):
+            self.param_list[idx]['inside box']='0.0 0.0 0.0 {} {} {}'.format(length,length,length)
 #        print self.param_list[idx]['inside box']
 #        raise NotImplementedError('Auto box size is not implemented yet!')
 
@@ -70,6 +83,11 @@ class PackmolRunner(object):
                 pm = pb.Molecule(a.openbabel_mol)
                 pm.write("pdb", filename=os.path.join(d, '{}.pdb'.format(idx)), overwrite=True)
 
+            # TODO: also check if user specified outside box, etc.
+            # Do not use auto mode if user specified any type of box
+            if 'inside box' not in self.param_list[idx]:
+                self._get_auto_boxsize()
+
             with open(os.path.join(d, 'pack.inp'), 'w') as inp:
                 # create packmol control file
                 inp.write('tolerance 2.0\n')
@@ -78,11 +96,6 @@ class PackmolRunner(object):
                 for idx, mol in enumerate(self.mols):
                     inp.write('\n')
                     inp.write('structure {}.pdb\n'.format(os.path.join(d, str(idx))))
-
-                    # TODO: also check if user specified outside box, etc.
-                    # Do not use auto mode if user specified any type of box
-                    if 'inside box' not in self.param_list[idx]:
-                        self._get_auto_boxsize(idx)
 
                     for k, v in self.param_list[idx].iteritems():
                         inp.write('  {} {}\n'.format(k, self._format_packmol_str(v)))
