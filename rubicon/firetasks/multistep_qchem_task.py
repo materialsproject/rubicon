e25dde0b77859fc21e72611cfa132fad05b09f95
Xiaohui Qu <xqu@lbl.gov>
2014-05-02 16:52:37 -0700

    make transfer of initial_charge case dependent to be compatible with ealier submitted workflows

--- rubicon/firetasks/multistep_qchem_task.py
+++ rubicon/firetasks/multistep_qchem_task.py
@@ -260,8 +260,9 @@ class QChemFrequencyDBInsertionTask(FireTaskBase, FWSerializable):
                     'offending_fwid': offending_fwid})
         molname = d['user_tags']['molname']
         mission = d['user_tags']['mission']
-        additional_user_tags = {"img_freq_eli": img_freq_eli,
-                                "initial_charge": fw_spec["user_tags"]["initial_charge"]}
+        additional_user_tags = {"img_freq_eli": img_freq_eli}
+        if "initial_charge" in fw_spec["user_tags"]:
+            additional_user_tags["initial_charge"] = fw_spec["user_tags"]["initial_charge"]
         priority = fw_spec['_priority']
         old_snl = EGStructureNL.from_dict(d['snl_initial'])
         history = old_snl.history
