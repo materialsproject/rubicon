0968b4ba99a074b357cc8611f51b53282d720a87
Xiaohui Qu <xqu@lbl.gov>
2014-05-20 16:05:45 -0700

    copy electrode potentials

--- rubicon/scripts/copy_molecules_collection.py
+++ rubicon/scripts/copy_molecules_collection.py
@@ -28,6 +28,7 @@ def transform_molecule_doc(mol1):
     mol2["reduced_cell_formula_abc"] = mol1["reduced_cell_formula_abc"]
     if "water" in mol1["solvated_properties"]:
         mol2["implicit_solvent"] = mol1["solvated_properties"]["water"]["implicit_solvent"]
+        mol2["electrode_potentials"] = mol1["solvated_properties"]["water"]["electrode_potentials"]
     else:
         mol2["implicit_solvent"] = {}
     mol2["pretty_formula"] = mol1["pretty_formula"]
