6f86bc4f6ed96c7b483dfa3d63c9163a70343f9a
Xiaohui Qu <xqu@lbl.gov>
2014-08-01 09:52:24 -0700

    reduce the upper limit of the sphere for metal to 3.0

--- rubicon/utils/ion_arranger/semi_emprical_qm_energy_evaluator.py
+++ rubicon/utils/ion_arranger/semi_emprical_qm_energy_evaluator.py
@@ -21,7 +21,7 @@ class SemiEmpricalQuatumMechanicalEnergyEvaluator(EnergyEvaluator):
 
     def __init__(self, ob_mol, ob_cation, ob_anion, total_charge=0,
                  lower_covalent_radius_scale=1.8, lower_metal_radius_scale=0.5,
-                 upper_covalent_radius_scale=4.0, upper_metal_radius_scale=4.0,):
+                 upper_covalent_radius_scale=4.0, upper_metal_radius_scale=3.0,):
         from rubicon.utils.ion_arranger.ion_arranger import IonPlacer
         mol_coords = IonPlacer.normalize_molecule(ob_mol)
         super(SemiEmpricalQuatumMechanicalEnergyEvaluator, self).__init__(mol_coords)
