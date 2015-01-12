95aad47d035839e029cfe21dc0660ab0abacc8c0
navnidhirajput <nnrajput@lbl.gov>
2015-01-12 14:48:41 -0800

    not testing lammps_output.py

--- rubicon/firetasks/lammps_output_task.py
+++ rubicon/firetasks/lammps_output_task.py
@@ -49,24 +49,24 @@ class WritelammpsOutputTask(FireTaskBase):
     _fw_name = "Lammps Output Writer"
 
 
-    def _insert_doc(self, fw_spec = None):
-        db_dir = shlex.os.environ['DB_LOC']
-        db_path = shlex.os.path.join(db_dir, 'tasks_db.json')
-        with open(db_path) as f:
-            db_creds = json.load(f)
-        conn = MongoClient(db_creds['host'], db_creds['port'],)
-        db = conn[db_creds['database']]
-        if db_creds['admin_user']:
-            db.authenticate(db_creds['admin_user'], db_creds['admin_password'])
-            coll = db['lammps_output']
-        parse_lammps = LammpsLog.from_file('mol.log')
-        docs = parse_lammps.llog
-        docs = {k: list(v) if isinstance(v, numpy.ndarray) else v for k, v in docs.items()}
-        coll.insert(docs)
-        #coll.update(docs)
-
-    def run_task(self, fw_spec):
-        self._insert_doc()
+    # def _insert_doc(self, fw_spec = None):
+    #     db_dir = shlex.os.environ['DB_LOC']
+    #     db_path = shlex.os.path.join(db_dir, 'tasks_db.json')
+    #     with open(db_path) as f:
+    #         db_creds = json.load(f)
+    #     conn = MongoClient(db_creds['host'], db_creds['port'],)
+    #     db = conn[db_creds['database']]
+    #     if db_creds['admin_user']:
+    #         db.authenticate(db_creds['admin_user'], db_creds['admin_password'])
+    #         coll = db['lammps_output']
+    #     parse_lammps = LammpsLog.from_file('mol.log')
+    #     docs = parse_lammps.llog
+    #     docs = {k: list(v) if isinstance(v, numpy.ndarray) else v for k, v in docs.items()}
+    #     coll.insert(docs)
+    #     #coll.update(docs)
+    #
+    # def run_task(self, fw_spec):
+    #     self._insert_doc()
 
 
 
