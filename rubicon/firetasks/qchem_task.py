41847f00ee23cf4ce24b4c4cf700ebb1890e9757
Xiaohui Qu <xqu@lbl.gov>
2015-02-19 15:10:41 -0800

    use 1/4 of CPU cores on Edison

--- rubicon/firetasks/qchem_task.py
+++ rubicon/firetasks/qchem_task.py
@@ -62,14 +62,14 @@ class QChemTask(FireTaskBase, FWSerializable):
                     "edique" in os.environ["PBS_JOBID"]):
         # hopper or edison compute nodes
             if (not fw_data.MULTIPROCESSING) or (fw_data.SUB_NPROCS is None):
-                qc_exe = shlex.split("qchem -np {}".format(min(24, len(mol))))
+                qc_exe = shlex.split("qchem -np {}".format(min(12, len(mol))))
                 half_cpus_cmd = shlex.split("qchem -np {}".format(
-                    min(12, len(mol))))
+                    min(6, len(mol))))
             else:
                 qc_exe = shlex.split("qchem -np {}".format(
-                    min(fw_data.SUB_NPROCS, len(mol))))
-                half_cpus_cmd = shlex.split("qchem -np {}".format(
                     min(fw_data.SUB_NPROCS/2, len(mol))))
+                half_cpus_cmd = shlex.split("qchem -np {}".format(
+                    min(fw_data.SUB_NPROCS/4, len(mol))))
         elif carver_name_pattern.match(socket.gethostname()):
         # mendel compute nodes
             qc_exe = shlex.split("qchem -np {}".format(min(8, len(mol))))
