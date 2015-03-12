4da0512dd5fbe248de629c2c4e1290c575688bb5
Xiaohui Qu <xqu@lbl.gov>
2015-03-12 13:35:19 -0700

    taboo other conformers temporarily

--- rubicon/utils/ion_arranger/semi_emprical_qm_energy_evaluator.py
+++ rubicon/utils/ion_arranger/semi_emprical_qm_energy_evaluator.py
@@ -45,7 +45,7 @@ class SemiEmpricalQuatumMechanicalEnergyEvaluator(EnergyEvaluator):
         self.ob_fragments = ob_fragments
         self.nums_fragments = nums_fragments
         self.run_number = 1
-        self.best_energy = 0.0
+        self.global_best_energy = 0.0
         self.memory_position_tolerance_au = 0.3 * AtomicRadiusUtils.angstrom2au
         self.memory_positions = []
         self.memory_size = 1000
@@ -54,6 +54,20 @@ class SemiEmpricalQuatumMechanicalEnergyEvaluator(EnergyEvaluator):
         self.taboo_tolerance_au = taboo_tolerance_ang * AtomicRadiusUtils.angstrom2au
         self.tabooed_raw_positions = []
         self.tabooed_optimizated_positions = []
+        self.current_best_raw_position = None
+        self.current_best_optimized_position = None
+        self.best_umbrella_ratio = 3.0
+        self.best_energy = None
+        self.best_mol = None
+        self.best_run_number = None
+
+    def save_current_best_position(self):
+        conformer_dir = "conformers"
+        if not os.path.exists(conformer_dir):
+            os.mkdir(conformer_dir)
+        mol_file_name = os.path.join(conformer_dir,
+                                     "step_{}_energy_{}.xyz".format(self.best_run_number, self.best_energy))
+        self.best_mol.to("xyz", mol_file_name)
 
     def taboo_current_position(self, raw_position_only=False):
         self.tabooed_raw_positions.append(list(itertools.chain(*self.current_raw_position)))
@@ -65,6 +79,13 @@ class SemiEmpricalQuatumMechanicalEnergyEvaluator(EnergyEvaluator):
                 final_coords = IonPlacer.normalize_molecule(final_mol)
                 self.current_optimized_position = final_coords
             self.tabooed_optimizated_positions.append(self.current_optimized_position)
+        self.save_current_best_position()
+        self.memory_positions = []
+        self.best_energy = None
+        self.current_best_optimized_position = None
+        self.current_best_raw_position = None
+        self.best_mol = None
+        self.best_run_number = None
 
     def is_current_position_tabooed(self, position_type):
         if position_type not in ["raw", "optimized"]:
@@ -84,29 +105,57 @@ class SemiEmpricalQuatumMechanicalEnergyEvaluator(EnergyEvaluator):
         return False
 
     def query_memory_positions(self, fragments_coords):
+        current_pos = list(itertools.chain(*fragments_coords))
         for p, energy in self.memory_positions:
             distance = max([math.sqrt(sum([(x1-x2)**2 for x1, x2 in zip(c1, c2)]))
                             for c1, c2 in
-                            zip(itertools.chain(*p), itertools.chain(*fragments_coords))])
+                            zip(p, current_pos)])
             if distance < self.memory_position_tolerance_au:
                 return energy
         return None
 
     def append_position_to_memory(self, fragments_coords, energy):
-        self.memory_positions.insert(0, tuple([fragments_coords, energy]))
+        self.memory_positions.insert(0, tuple([list(itertools.chain(*fragments_coords)), energy]))
         if len(self.memory_positions) > self.memory_size:
             self.memory_positions.pop()
 
+    def is_raw_position_inside_best_umbrella(self, fragments_coords):
+        current_pos = list(itertools.chain(*fragments_coords))
+        distance = max([math.sqrt(sum([(x1-x2)**2 for x1, x2 in zip(c1, c2)]))
+                        for c1, c2 in
+                        zip(self.current_best_raw_position, current_pos)])
+        if distance < self.taboo_tolerance_au * self.best_umbrella_ratio:
+            return True
+        else:
+            return False
+
+    def is_optimized_position_inside_the_best_promixity(self, fragments_coords):
+        if self.current_best_optimized_position is None:
+            return True
+        distance = max([math.sqrt(sum([(x1-x2)**2 for x1, x2 in zip(c1, c2)]))
+                        for c1, c2 in
+                        zip(self.current_best_optimized_position, fragments_coords)])
+        if distance < self.taboo_tolerance_au:
+            return True
+        else:
+            return False
+
     def calc_energy(self, fragments_coords):
+        tabooed_energy = 10.0
+        unmbrella_taboo_energy = 10.0
+
         self.current_raw_position = fragments_coords
         self.current_optimized_position = None
         if self.is_current_position_tabooed(position_type="raw"):
-            return 1.0
+            return tabooed_energy
+        unmbrella_enhancement = 0.0
+        if self.is_raw_position_inside_best_umbrella(fragments_coords):
+            unmbrella_enhancement = -3.0
         energy = self.lower_sphere.calc_energy(fragments_coords)
         if energy > HardSphereEnergyEvaluator.overlap_energy * 0.9:
-            return energy
+            return energy + unmbrella_enhancement
         if not self.contact_detector.is_contact(fragments_coords):
-            return self.gravitation.calc_energy(fragments_coords)
+            return self.gravitation.calc_energy(fragments_coords) + unmbrella_enhancement
         memorized_energy = self.query_memory_positions(fragments_coords)
         if memorized_energy is not None:
             energy = memorized_energy
@@ -118,8 +167,17 @@ class SemiEmpricalQuatumMechanicalEnergyEvaluator(EnergyEvaluator):
             self.current_optimized_position = final_coords
             if self.is_current_position_tabooed(position_type="optimized"):
                 self.taboo_current_position(raw_position_only=True)
-                return 1.0
+                return tabooed_energy
             energy = round(energy, 3)
+            if self.is_optimized_position_inside_the_best_promixity(final_coords):
+                if energy < self.best_energy or self.current_best_optimized_position is None:
+                    self.best_energy = energy
+                    self.current_best_optimized_position = final_coords
+                    self.current_best_raw_position = list(itertools.chain(*fragments_coords))
+                    self.best_mol = final_mol
+                    self.best_run_number = self.run_number
+                else:
+                    energy = unmbrella_taboo_energy
             self.append_position_to_memory(fragments_coords, energy)
         # coarse grained energy,
         # make potential energy surface simpler
@@ -144,8 +202,8 @@ class SemiEmpricalQuatumMechanicalEnergyEvaluator(EnergyEvaluator):
             final_pmg_mol = mopout.data["molecules"][-1]
             final_ob_mol = BabelMolAdaptor(final_pmg_mol)._obmol
             energy = Energy(mopout.data["energies"][-1][-1], "eV").to("Ha")
-            if energy < self.best_energy:
-                self.best_energy = energy
+            if energy < self.global_best_energy:
+                self.global_best_energy = energy
                 shutil.copy("mol.out", os.path.join(cur_dir, "best_mol.out"))
             for run in custodian_out:
                 for correction in run['corrections']:
