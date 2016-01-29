be2face977b7cc0c663a5358f5b543d4a37c8ccf
navnidhirajput <nnrajput@lbl.gov>
2016-01-29 15:53:26 -0800

    freq wf

--- rubicon/firetasks/gaussian_fw.py
+++ rubicon/firetasks/gaussian_fw.py
@@ -13,6 +13,7 @@ from pymatgen.io import gaussian
 
 import glob
 from pymatgen import Molecule
+import os
 
 
 if __name__ == '__main__':
@@ -25,10 +26,13 @@ if __name__ == '__main__':
     for filename in moleculelist:
         mol = Molecule.from_file(filename)
 
-        fw1 = Firework([task_geo],name = 'Gaussian geometry optimization', fw_id=1, spec= {"charge": 1,"spin_multiplicity":-1})
+        file_name = os.path.basename(filename)
+
+        fw1 = Firework([task_geo],name = 'Gaussian geometry optimization', fw_id=1, spec= {"molecule":mol, "mol_name": os.path.splitext(file_name)[0], "charge": 0,"spin_multiplicity":1})
         fw2 = Firework([task_freq],name='Gaussian frequency and charge', fw_id=2, spec= {"charge": 1,"spin_multiplicity":-1})
 
         depen = {1:[2]}
+        wf = Workflow([fw1], name="GAUSSIAN")
         wf = Workflow([fw1,fw2], name="GAUSSIAN", links_dict=depen)
 
         lp = LaunchPad.auto_load()
