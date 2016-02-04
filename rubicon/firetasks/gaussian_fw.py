5c33c761206e9e2da8ebaeb9570417d47073d2b9
navnidhirajput <nnrajput@lbl.gov>
2016-02-04 10:33:17 -0800

    GaussionGeoTask

--- rubicon/firetasks/gaussian_fw.py
+++ rubicon/firetasks/gaussian_fw.py
@@ -19,7 +19,7 @@ import os
 if __name__ == '__main__':
 
     task_geo = WritegaussianGeoTask()
-    task_freq = WritegaussianFreqTask()
+    #task_freq = WritegaussianFreqTask()
     coords = []
     sp = []
     moleculelist = glob.glob("/Users/navnidhirajput/Dropbox/solvent_molecules/*")
@@ -29,11 +29,11 @@ if __name__ == '__main__':
         file_name = os.path.basename(filename)
 
         fw1 = Firework([task_geo],name = 'Gaussian geometry optimization', fw_id=1, spec= {"molecule":mol, "mol_name": os.path.splitext(file_name)[0], "charge": 0,"spin_multiplicity":1})
-        fw2 = Firework([task_freq],name='Gaussian frequency and charge', fw_id=2, spec= {"molecule":mol, "mol_name": os.path.splitext(file_name)[0], "charge": 0,"spin_multiplicity":1})
+        #fw2 = Firework([task_freq],name='Gaussian frequency and charge', fw_id=2, spec= {"molecule":mol, "mol_name": os.path.splitext(file_name)[0], "charge": 0,"spin_multiplicity":1})
 
-        depen = {1:[2]}
+        #depen = {1:[2]}
         wf = Workflow([fw1], name="GAUSSIAN")
-        wf = Workflow([fw1,fw2], name="GAUSSIAN", links_dict=depen)
+        #wf = Workflow([fw1,fw2], name="GAUSSIAN", links_dict=depen)
 
         lp = LaunchPad.auto_load()
         lp.add_wf(wf)
