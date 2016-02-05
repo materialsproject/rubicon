68d7576b7a2911393c06f5abc243ce04862a446d
navnidhirajput <nnrajput@lbl.gov>
2016-02-04 16:14:38 -0800

    2 on 3 depen

--- rubicon/firetasks/gaussian_fw.py
+++ rubicon/firetasks/gaussian_fw.py
@@ -1,6 +1,7 @@
 from rubicon.firetasks.multistep_gauss_task import \
     GaussianGeomOptDBInsertionTask
-from rubicon.gaussian.gaussian_input_task import WritegaussianGeoTask
+from rubicon.gaussian.gaussian_input_task import WritegaussianGeoTask, \
+    WritegaussianFreqESPTask
 
 __author__ = 'navnidhirajput'
 
@@ -21,6 +22,7 @@ if __name__ == '__main__':
 
     task_geo = WritegaussianGeoTask()
     task_geo_dbinsert = GaussianGeomOptDBInsertionTask()
+    task_freq_esp = WritegaussianFreqESPTask()
     coords = []
     sp = []
     moleculelist = glob.glob("/Users/navnidhirajput/Dropbox/solvent_molecules/*")
@@ -31,10 +33,11 @@ if __name__ == '__main__':
 
         fw1 = Firework([task_geo],name = 'Gaussian geometry optimization', spec= {"molecule":mol, "mol_name": os.path.splitext(file_name)[0], "charge": 0,"spin_multiplicity":1}, fw_id=1)
         fw2 = Firework([task_geo_dbinsert],name='Gaussian Geometry DB insertion', spec= {"molecule":mol, "mol_name": os.path.splitext(file_name)[0], "charge": 0,"spin_multiplicity":1}, fw_id=2)
+        fw3 = Firework([task_freq_esp],name='Gaussian Frequency and ESP', spec= {"mol_name": os.path.splitext(file_name)[0], "charge": 0,"spin_multiplicity":1}, fw_id=3)
 
-        depen = {1:[2]}
+        depen = {1:[2,3], 2:3}
         #wf = Workflow([fw1], name="GAUSSIAN")
-        wf = Workflow([fw1,fw2], name="GAUSSIAN", links_dict=depen)
+        wf = Workflow([fw1,fw2,fw3], name="GAUSSIAN", links_dict=depen)
 
         lp = LaunchPad.auto_load()
         lp.add_wf(wf)
