6b092bab20fc12fb65e7291f224c69df0d88bdc7
navnidhirajput <nnrajput@lbl.gov>
2016-02-04 16:15:52 -0800

    2 on 3 depen

--- rubicon/firetasks/gaussian_fw.py
+++ rubicon/firetasks/gaussian_fw.py
@@ -35,7 +35,7 @@ if __name__ == '__main__':
         fw2 = Firework([task_geo_dbinsert],name='Gaussian Geometry DB insertion', spec= {"molecule":mol, "mol_name": os.path.splitext(file_name)[0], "charge": 0,"spin_multiplicity":1}, fw_id=2)
         fw3 = Firework([task_freq_esp],name='Gaussian Frequency and ESP', spec= {"mol_name": os.path.splitext(file_name)[0], "charge": 0,"spin_multiplicity":1}, fw_id=3)
 
-        depen = {1:[2,3], 2:3}
+        depen = {1:2, 2:3}
         #wf = Workflow([fw1], name="GAUSSIAN")
         wf = Workflow([fw1,fw2,fw3], name="GAUSSIAN", links_dict=depen)
 
