08800c694206ec5e6d9e6a0864ec65468d252e74
navnidhirajput <nnrajput@lbl.gov>
2016-02-08 15:09:49 -0800

    instead of pdb run from gaussian output

--- rubicon/gff/antechamberio.py
+++ rubicon/gff/antechamberio.py
@@ -4,7 +4,7 @@ from rubicon.gff.ffmol import FFmol
 __author__ = 'navnidhirajput'
 
 """ A wrapper for AntechamberRunner which generates force field files
-    for a specified molecule using pdb file as input
+    for a specified molecule using gaussian output file as input
 """
 
 import subprocess
@@ -13,7 +13,7 @@ import shlex
 from gff import Gff, FFCorruptionException, correct_corrupted_frcmod_files
 from monty.tempfile import ScratchDir
 import tempfile
-from rubicon.gff.topology import TopMol, TopCorruptionException, TopCorruptionException, correct_corrupted_top_files
+from rubicon.gff.topology import TopMol, TopCorruptionException, correct_corrupted_top_files
 
 
 class AntechamberRunner():
@@ -53,12 +53,12 @@ class AntechamberRunner():
 
     def get_ff_top_mol(self, mol, filename=None):
         """
-        run antechamber using pdb file then run parmchk
+        run antechamber using gaussian output file then run parmchk
         to generate missing force field parameters. Store and return
         the force field and topology information in ff_mol.
 
         Args:
-            filename: pdb file of the molecule
+            filename: gaussian output file of the molecule
             mol: molecule
         Returns:
             ff_mol : Object of FFmol which contains information of
@@ -69,10 +69,10 @@ class AntechamberRunner():
         with ScratchDir(scratch, copy_from_current_on_enter=True,
                         copy_to_current_on_exit=True) as d:
 
-            self._convert_to_pdb(mol, 'mol.pdb')
+            #self._convert_to_pdb(mol, 'mol.pdb')
             command = (
-                'antechamber -i ' + filename + ' -fi pdb -o ' + filename[:-4] +
-                " -fo charmm")
+                'antechamber -i ' + filename + ' -fi gout -o ' + filename[:-4] +
+                " -fo charmm -c resp -s 2 >& runinfo")
             return_cmd = subprocess.call(shlex.split(command))
             self.molname = filename.split('.')[0]
             self._run_parmchk()
