6d9f6c1922dfbe898c77e52f3ff984bf03325168
navnidhirajput <nnrajput@lbl.gov>
2016-02-08 13:51:23 -0800

    freq DB insertion wf

--- rubicon/firetasks/gaussian_fw.py
+++ rubicon/firetasks/gaussian_fw.py
@@ -1,5 +1,5 @@
 from rubicon.firetasks.multistep_gauss_task import \
-    GaussianGeomOptDBInsertionTask
+    GaussianGeomOptDBInsertionTask, GaussianFreqESPDBInsertionTask
 from rubicon.gaussian.gaussian_input_task import WritegaussianGeoTask, \
     WritegaussianFreqESPTask
 
@@ -23,21 +23,21 @@ if __name__ == '__main__':
     task_geo = WritegaussianGeoTask()
     task_geo_dbinsert = GaussianGeomOptDBInsertionTask()
     task_freq_esp = WritegaussianFreqESPTask()
+    task_freq_esp_dbinsert = GaussianFreqESPDBInsertionTask()
     coords = []
     sp = []
     moleculelist = glob.glob("/Users/navnidhirajput/Dropbox/solvent_molecules/*")
     for filename in moleculelist:
         mol = Molecule.from_file(filename)
-
         file_name = os.path.basename(filename)
-
         fw1 = Firework([task_geo],name = 'Gaussian geometry optimization', spec= {"molecule":mol, "mol_name": os.path.splitext(file_name)[0], "charge": 0,"spin_multiplicity":1}, fw_id=1)
         fw2 = Firework([task_geo_dbinsert],name='Gaussian Geometry DB insertion', spec= {"molecule":mol, "mol_name": os.path.splitext(file_name)[0], "charge": 0,"spin_multiplicity":1}, fw_id=2)
         fw3 = Firework([task_freq_esp],name='Gaussian Frequency and ESP', spec= {"mol_name": os.path.splitext(file_name)[0], "charge": 0,"spin_multiplicity":1}, fw_id=3)
+        fw4 = Firework([task_freq_esp_dbinsert],name='Gaussian Frequency and ESP DB insertion', spec= {"molecule":mol, "mol_name": os.path.splitext(file_name)[0], "charge": 0,"spin_multiplicity":1}, fw_id=4)
 
-        depen = {1:2, 2:3}
-        #wf = Workflow([fw1], name="GAUSSIAN")
-        wf = Workflow([fw1,fw2,fw3], name="GAUSSIAN", links_dict=depen)
+        depen = {1:2, 2:3,3:4}
+
+        wf = Workflow([fw1,fw2,fw3, fw4], name="GAUSSIAN", links_dict=depen)
 
         lp = LaunchPad.auto_load()
         lp.add_wf(wf)
