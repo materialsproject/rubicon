482f40d76c9990dd1fbee370e64354fa9c2ac497
navnidhirajput <nnrajput@lbl.gov>
2016-02-11 16:17:24 -0800

    testing wf

--- rubicon/firetasks/gaussian_fw.py
+++ rubicon/firetasks/gaussian_fw.py
@@ -36,12 +36,15 @@ if __name__ == '__main__':
         fw2 = Firework([task_geo_dbinsert],name='Gaussian Geometry DB insertion', spec= {"molecule":mol, "mol_name": os.path.splitext(file_name)[0], "charge": 0,"spin_multiplicity":1}, fw_id=2)
         fw3 = Firework([task_freq_esp],name='Gaussian Frequency and ESP', spec= {"mol_name": os.path.splitext(file_name)[0], "charge": 0,"spin_multiplicity":1}, fw_id=3)
         #fw4 = Firework([task_freq_esp_dbinsert],name='Gaussian Frequency and ESP DB insertion', spec= {"molecule":mol, "mol_name": os.path.splitext(file_name)[0], "charge": 0,"spin_multiplicity":1}, fw_id=4)
-        fw4 = Firework([task_lammps_inp],name = 'Run Lammps', spec={"molecule":mol, "mol_name": os.path.splitext(file_name)[0]}, fw_id=4)
+        #fw4 = Firework([task_lammps_inp],name = 'Run Lammps', spec={"molecule":mol, "mol_name": os.path.splitext(file_name)[0]}, fw_id=4)
+        fw4 = Firework([task_lammps_inp],name = 'Run Lammps', spec={"molecule":mol, "mol_name": [mol]*len(coords)}, fw_id=4)
 
 
         depen = {1:2, 2:3,3:4}
-
         wf = Workflow([fw1,fw2,fw3, fw4], name="GAUSSIAN", links_dict=depen)
+
+        #depen = {1:2, 2:3}
+        #wf = Workflow([fw1,fw2,fw3], name="GAUSSIAN", links_dict=depen)
 
         lp = LaunchPad.auto_load()
         lp.add_wf(wf)
