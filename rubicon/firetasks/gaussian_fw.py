aadd195437acd4dadf51cdc3d3c6c37f3890fd49
navnidhirajput <nnrajput@lbl.gov>
2016-02-12 16:14:45 -0800

    testing wf

--- rubicon/firetasks/gaussian_fw.py
+++ rubicon/firetasks/gaussian_fw.py
@@ -32,12 +32,15 @@ if __name__ == '__main__':
     for filename in moleculelist:
         mol = Molecule.from_file(filename)
         file_name = os.path.basename(filename)
+        #print "++",[os.path.splitext(file_name)[0]]*len(mol.cart_coords)
+        mol_with_site_prop = Molecule(mol.species, mol.cart_coords,site_properties={"mol_name":[os.path.splitext(file_name)[0]]*len(mol.cart_coords)})
+        print mol_with_site_prop.site_properties["mol_name"]
         fw1 = Firework([task_geo],name = 'Gaussian geometry optimization', spec= {"molecule":mol, "mol_name": os.path.splitext(file_name)[0], "charge": 0,"spin_multiplicity":1}, fw_id=1)
         fw2 = Firework([task_geo_dbinsert],name='Gaussian Geometry DB insertion', spec= {"molecule":mol, "mol_name": os.path.splitext(file_name)[0], "charge": 0,"spin_multiplicity":1}, fw_id=2)
         fw3 = Firework([task_freq_esp],name='Gaussian Frequency and ESP', spec= {"mol_name": os.path.splitext(file_name)[0], "charge": 0,"spin_multiplicity":1}, fw_id=3)
         #fw4 = Firework([task_freq_esp_dbinsert],name='Gaussian Frequency and ESP DB insertion', spec= {"molecule":mol, "mol_name": os.path.splitext(file_name)[0], "charge": 0,"spin_multiplicity":1}, fw_id=4)
         #fw4 = Firework([task_lammps_inp],name = 'Run Lammps', spec={"molecule":mol, "mol_name": os.path.splitext(file_name)[0]}, fw_id=4)
-        fw4 = Firework([task_lammps_inp],name = 'Run Lammps', spec={"molecule":mol, "mol_name": [mol]*len(coords)}, fw_id=4)
+        fw4 = Firework([task_lammps_inp],name = 'Run Lammps', spec={"molecule":mol_with_site_prop}, fw_id=4)
 
 
         depen = {1:2, 2:3,3:4}
