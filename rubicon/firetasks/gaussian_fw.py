22bc57a44083216d4dbb5995d3e34917e9f1637a
navnidhirajput <nnrajput@lbl.gov>
2016-02-16 10:20:10 -0800

    lammps log db insertiong wf

--- rubicon/firetasks/gaussian_fw.py
+++ rubicon/firetasks/gaussian_fw.py
@@ -1,4 +1,5 @@
 from rubicon.firetasks.lammps_input_task import WritelammpsInputTask
+from rubicon.firetasks.lammps_output_task import WritelammpsOutputTask
 from rubicon.firetasks.multistep_gauss_task import \
     GaussianGeomOptDBInsertionTask, GaussianFreqESPDBInsertionTask
 from rubicon.gaussian.gaussian_input_task import WritegaussianGeoTask, \
@@ -26,6 +27,7 @@ if __name__ == '__main__':
     task_freq_esp = WritegaussianFreqESPTask()
     task_freq_esp_dbinsert = GaussianFreqESPDBInsertionTask()
     task_lammps_inp = WritelammpsInputTask()
+    task_lammps_log_dbinsert= WritelammpsOutputTask()
     coords = []
     sp = []
     moleculelist = glob.glob("/Users/navnidhirajput/Dropbox/solvent_molecules/*")
@@ -37,13 +39,11 @@ if __name__ == '__main__':
         fw1 = Firework([task_geo],name = 'Gaussian geometry optimization', spec= {"molecule":mol, "mol_name": os.path.splitext(file_name)[0], "charge": 0,"spin_multiplicity":1}, fw_id=1)
         fw2 = Firework([task_geo_dbinsert],name='Gaussian Geometry DB insertion', spec= {"molecule":mol, "mol_name": os.path.splitext(file_name)[0], "charge": 0,"spin_multiplicity":1}, fw_id=2)
         fw3 = Firework([task_freq_esp],name='Gaussian Frequency and ESP', spec= {"mol_name": os.path.splitext(file_name)[0], "charge": 0,"spin_multiplicity":1}, fw_id=3)
-        #fw4 = Firework([task_freq_esp_dbinsert],name='Gaussian Frequency and ESP DB insertion', spec= {"molecule":mol, "mol_name": os.path.splitext(file_name)[0], "charge": 0,"spin_multiplicity":1}, fw_id=4)
-        #fw4 = Firework([task_lammps_inp],name = 'Run Lammps', spec={"molecule":mol, "mol_name": os.path.splitext(file_name)[0]}, fw_id=4)
         fw4 = Firework([task_lammps_inp],name = 'Run Lammps', spec={"molecule":mol_with_site_prop}, fw_id=4)
+        fw5 = Firework([task_lammps_log_dbinsert],name='Lammps Log Parsing', fw_id=5)
 
-
-        depen = {1:2, 2:3,3:4}
-        wf = Workflow([fw1,fw2,fw3, fw4], name="GAUSSIAN", links_dict=depen)
+        depen = {1:2, 2:3,3:4,4:5}
+        wf = Workflow([fw1,fw2,fw3, fw4,fw5], name="GAUSSIAN", links_dict=depen)
 
 
         lp = LaunchPad.auto_load()
