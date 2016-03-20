1da0380f9fc6a56cb80bfa19dd9964e4e7ca1b85
Kiran Mathew <km468@cornell.edu>
2016-03-19 18:09:46 -0700

    refactor packmol.py

--- rubicon/packmol/packmol.py
+++ rubicon/packmol/packmol.py
@@ -28,13 +28,20 @@ class PackmolRunner(object):
               "https://github.com/leandromartinez98/packmol "
               "and follow the instructions in the README to compile. "
               "Don't forget to add the packmol binary to your path")
-    def __init__(self, mols, param_list, boxit=True):
+    def __init__(self, mols, param_list,
+                 input_file="pack.inp",
+                 control_params={"tolerance": 2.0, "filetype": "pdb"},
+                 boxit=True):
         """
         Create PackMolRunner
 
         Args:
               mols:
                    list of Molecules to pack
+              input_file:
+                        name of the packmol input file
+              control_params:
+                           packmol control parameters dictionary
               param_list:
                     list of parameters containing dicts for each molecule
               boxit:
@@ -42,29 +49,33 @@ class PackmolRunner(object):
         """
         self.mols = mols
         self.param_list = param_list
+        self.input_file = input_file
         self.boxit = boxit
+        self.control_params = control_params
+        if not self.control_params.get("output"):
+            self.control_params["output"] = "packed."+self.control_params["filetype"]
+        if self.boxit:
+            self._set_box()
 
-    def _format_packmol_str(self, some_obj):
+    def _format_param_val(self, param_val):
         """
-        Internal method to format packmol strings
+        Internal method to format values in the packmol parameter dictionaries
 
         Args:
-              some_obj:
+              param_val:
                    Some object to turn into String
 
         Returns:
                string representation of the object
         """
-        if isinstance(some_obj, list):
-            return ' '.join(str(x) for x in some_obj)
+        if isinstance(param_val, list):
+            return ' '.join(str(x) for x in param_val)
         else:
-            return some_obj
+            return str(param_val)
 
-    def _get_auto_boxsize(self):
+    def _set_box(self):
         """
-        TODO: Put docs here!!
-        :param idx:
-        :raise NotImplementedError:
+        Box the molecule assemblies
         """
         volume = 0.0
         for idx, mol in enumerate(self.mols):
@@ -88,48 +99,56 @@ class PackmolRunner(object):
         """
         scratch = tempfile.gettempdir()
         with ScratchDir(scratch, copy_to_current_on_exit=True) as d:
-            # convert mols to pdb files
-            for idx, mol in enumerate(self.mols):
-                a = BabelMolAdaptor(mol)
-                pm = pb.Molecule(a.openbabel_mol)
-                pm.write("pdb", filename=os.path.join(d, '{}.pdb'.format(idx)),
-                         overwrite=True)
-            # TODO: also check if user specified outside box, etc.
-            # Do not use auto mode if user specified any type of box
-            if 'inside box' not in self.param_list[idx] and self.boxit:
-                self._get_auto_boxsize()
-            with open(os.path.join(d, 'pack.inp'), 'w') as inp:
-                # create packmol control file
-                inp.write('tolerance 2.0\n')
-                inp.write('filetype pdb\n')
-                inp.write('output {}\n'.format(os.path.join(d, "box.pdb")))
+            with open(os.path.join(d, self.input_file), 'w') as inp:
+                for k, v in self.control_params.iteritems():
+                    inp.write('{} {}\n'.format(k, self._format_param_val(v)))
                 for idx, mol in enumerate(self.mols):
+                    a = BabelMolAdaptor(mol)
+                    pm = pb.Molecule(a.openbabel_mol)
+                    pm.write(self.control_params["filetype"],
+                             filename=os.path.join(d, '{}.{}'.format(idx,
+                                                                     self.control_params["filetype"])),
+                             overwrite=True)
                     inp.write('\n')
                     inp.write(
-                        'structure {}.pdb\n'.format(os.path.join(d, str(idx))))
-
+                        'structure {}.{}\n'.format(os.path.join(d, str(idx)),
+                                                   self.control_params["filetype"]))
                     for k, v in self.param_list[idx].iteritems():
                         inp.write(
-                            '  {} {}\n'.format(k, self._format_packmol_str(v)))
+                            '  {} {}\n'.format(k, self._format_param_val(v)))
                     inp.write('end structure\n')
             proc = Popen(['packmol'],
-                         stdin=open(os.path.join(d, 'pack.inp'), 'r'),
+                         stdin=open(os.path.join(d, self.input_file), 'r'),
                          stdout=PIPE)
             (stdout, stderr) = proc.communicate()
-            a = BabelMolAdaptor.from_file(os.path.join(d, "box.pdb"), "pdb")
-            return a.pymatgen_mol
+            output_file = os.path.join(d, self.control_params["output"])
+            if os.path.isfile(output_file):
+                a = BabelMolAdaptor.from_file(output_file)
+                print "packed molecule written to {}".format(self.control_params["output"])
+                return a.pymatgen_mol
+            else:
+                print "Packmol execution failed"
+                print stdout, stderr
 
 
 if __name__ == '__main__':
-    coords = [[0.000000, 0.000000, 0.000000],
-              [0.000000, 0.000000, 1.089000],
-              [1.026719, 0.000000, -0.363000],
-              [-0.513360, -0.889165, -0.363000],
-              [-0.513360, 0.889165, -0.363000]]
-    mol = Molecule(["C", "H", "H", "H", "H"], coords)
-    #    pmr = PackmolRunner([mol, mol], [{"number":4,"inside box":[0.,0.,0.,40.,40.,40.]}, {"number":5, "inside box":[0.,0.,0.,40.,40.,40.]}])
-    pmr = PackmolRunner([mol, mol], [
-        {"number": 4, "inside box": [0., 0., 0., 40., 40., 40.]},
-        {"number": 5}])
+    ethanol_coords = [[0.00720, -0.56870, 0.00000],
+                      [-1.28540, 0.24990, 0.00000],
+                      [1.13040, 0.31470, 0.00000],
+                      [0.03920, -1.19720, 0.89000],
+                      [0.03920, -1.19720, -0.89000],
+                      [-1.31750, 0.87840, 0.89000],
+                      [-1.31750, 0.87840, -0.89000],
+                      [-2.14220, -0.42390, -0.00000],
+                      [1.98570, -0.13650, -0.00000]]
+    ethanol = Molecule(["C", "C","O","H","H","H","H","H","H"], ethanol_coords)
+    water_coords = [[9.626,6.787,12.673],
+                    [9.626, 8.420, 12.673],
+                    [10.203, 7.604, 12.673]]
+    water = Molecule(["H", "H", "O"], water_coords)
+    pmr = PackmolRunner([ethanol, water],
+                        [{"number": 1, "fixed":[0,0,0,0,0,0], "centerofmass":""},
+                         {"number": 15, "inside sphere": [0, 0, 0, 5]} ],
+                        control_params = {"tolerance": 2.0, "filetype": "xyz"},
+                        boxit=False)
     s = pmr.run()
-    print s
