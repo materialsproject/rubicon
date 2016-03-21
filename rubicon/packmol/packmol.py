f825dfbe7489f34b647f1b9808bea43ea05ab5df
Kiran Mathew <km468@cornell.edu>
2016-03-21 08:31:34 -0700

    further refactor packmol

--- rubicon/packmol/packmol.py
+++ rubicon/packmol/packmol.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python
+# coding: utf-8
+
 import os
 import tempfile
 from subprocess import Popen, PIPE
@@ -22,16 +23,18 @@ class PackmolRunner(object):
     """
     Create MD simulation box using packmol.
     """
+
     @requires(which('packmol'),
               "PackmolRunner requires the executable 'packmol' to be in "
               "the path. Please download packmol from "
               "https://github.com/leandromartinez98/packmol "
               "and follow the instructions in the README to compile. "
               "Don't forget to add the packmol binary to your path")
-    def __init__(self, mols, param_list,
-                 input_file="pack.inp",
-                 control_params={"tolerance": 2.0, "filetype": "pdb"},
-                 boxit=True):
+
+    def __init__(self, mols, param_list, input_file="pack.inp",
+                 tolerance=2.0, filetype="xyz",
+                 control_params={"maxit":20, "nloop":600},
+                 boxit=True, output_file="packed.xyz"):
         """
         Create PackMolRunner
 
@@ -40,20 +43,34 @@ class PackmolRunner(object):
                    list of Molecules to pack
               input_file:
                         name of the packmol input file
+              tolerance:
+                        min distance between the atoms
+              filetype:
+                       input/output structure file type
               control_params:
-                           packmol control parameters dictionary
+                           packmol control parameters dictionary. Basically
+                           all parameters other than structure/atoms
               param_list:
                     list of parameters containing dicts for each molecule
               boxit:
                     put the molecule assembly in a box
+              output_file:
+                    output file name. The extension will be adjusted
+                    according to the filetype
         """
         self.mols = mols
         self.param_list = param_list
         self.input_file = input_file
         self.boxit = boxit
         self.control_params = control_params
+        if not self.control_params.get("tolerance"):
+            self.control_params["tolerance"] = tolerance
+        if not self.control_params.get("filetype"):
+            self.control_params["filetype"] = filetype
         if not self.control_params.get("output"):
-            self.control_params["output"] = "packed."+self.control_params["filetype"]
+            self.control_params["output"] = \
+                "{}.{}".format(output_file.split(".")[0],
+                               self.control_params["filetype"])
         if self.boxit:
             self._set_box()
 
@@ -75,7 +92,7 @@ class PackmolRunner(object):
 
     def _set_box(self):
         """
-        Box the molecule assemblies
+        Box the molecule assembly
         """
         volume = 0.0
         for idx, mol in enumerate(self.mols):
@@ -123,12 +140,13 @@ class PackmolRunner(object):
             (stdout, stderr) = proc.communicate()
             output_file = os.path.join(d, self.control_params["output"])
             if os.path.isfile(output_file):
-                a = BabelMolAdaptor.from_file(output_file)
+                packed_mol = BabelMolAdaptor.from_file(output_file)
                 print "packed molecule written to {}".format(self.control_params["output"])
-                return a.pymatgen_mol
+                return packed_mol.pymatgen_mol
             else:
                 print "Packmol execution failed"
                 print stdout, stderr
+                return None
 
 
 if __name__ == '__main__':
@@ -141,7 +159,8 @@ if __name__ == '__main__':
                       [-1.31750, 0.87840, -0.89000],
                       [-2.14220, -0.42390, -0.00000],
                       [1.98570, -0.13650, -0.00000]]
-    ethanol = Molecule(["C", "C","O","H","H","H","H","H","H"], ethanol_coords)
+    ethanol = Molecule(["C", "C", "O", "H", "H", "H", "H", "H", "H"],
+                       ethanol_coords)
     water_coords = [[9.626,6.787,12.673],
                     [9.626, 8.420, 12.673],
                     [10.203, 7.604, 12.673]]
@@ -149,6 +168,8 @@ if __name__ == '__main__':
     pmr = PackmolRunner([ethanol, water],
                         [{"number": 1, "fixed":[0,0,0,0,0,0], "centerofmass":""},
                          {"number": 15, "inside sphere": [0, 0, 0, 5]} ],
-                        control_params = {"tolerance": 2.0, "filetype": "xyz"},
-                        boxit=False)
+                        input_file="packmol_input.inp", tolerance=2.0,
+                        filetype="xyz",
+                        control_params = {"nloop": 1000},
+                        boxit=False, output_file="cocktail.xyz")
     s = pmr.run()
