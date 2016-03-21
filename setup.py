0482f96bbf035b1138f8486e5fb7a25dff198836
Kiran Mathew <km468@cornell.edu>
2016-03-21 09:49:29 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,4 @@
-#!/usr/bin/env python
-
-__author__ = "Anubhav Jain"
-__copyright__ = "Copyright 2013, The Materials Project"
-__version__ = "0.1"
-__maintainer__ = "Anubhav Jain"
-__email__ = "ajain@lbl.gov"
-__date__ = "Apr 10, 2013"
+# coding: utf-8
 
 import os
 import sys
@@ -17,7 +10,7 @@ try:
     from numpy.distutils.core import setup, Extension
 except ImportError:
     print "numpy.distutils.core cannot be imported. Install numpy"
-    sys.exit(-1)
+    sys.exit(-9)
 
 from rubicon import __version__
 
@@ -30,17 +23,21 @@ md_analyzer = Extension(name = 'rubicon.analysis.lammps._md_analyzer',
 if __name__ == "__main__":
     setup(name='rubicon',
           version=__version__,
-          description='codes for JCESR / Electrolyte Genome',
+          description='Codes for JCESR / Electrolyte Genome',
           long_description=open(os.path.join(module_dir, 'README.rst')).read(),
           url='https://github.com/materialsproject/rubicon',
-          author='Anubhav Jain',
-          author_email='anubhavster@gmail.com',
+          author='Xiaohui Qu, Navnidhi Rajput, Kiran Mathew, Michael Humbert, '
+                 'Zhang Yong, Shyue Ping Ong, Anubhav Jain',
+          author_email='xqu@lbl.gov',
           license='modified BSD',
           packages=find_packages(),
-          zip_safe=False,
           install_requires=['pymatgen>=3.0', 'fireworks>=0.9',
                             'custodian>=0.7', 'monty>=0.7.0'],
-          extras_require={"plotting": ["matplotlib>=1.1"]},
+          extras_require={"plotting": ["matplotlib>=1.1"],
+                          "molecules": ["openbabel"],
+                          "molecular dynamics": ["packmol"]},
+          keywords=["materials", "project", "electrolyte", "molecular dynamics",
+                    "lammps", "qchem", "analysis"],
           classifiers=["Programming Language :: Python :: 2.7",
                        "Development Status :: 2 - Pre-Alpha",
                        "Intended Audience :: Science/Research",
@@ -49,8 +46,6 @@ if __name__ == "__main__":
                        "Operating System :: OS Independent",
                        "Topic :: Other/Nonlisted Topic",
                        "Topic :: Scientific/Engineering"],
-          test_suite='nose.collector',
-          tests_require=['nose'],
           ext_modules=[md_analyzer],
           scripts=[os.path.join(os.path.join(module_dir, "scripts", f))
                    for f in os.listdir(os.path.join(module_dir, "scripts"))])
