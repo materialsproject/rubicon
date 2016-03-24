da45dc8747c9bd24395aace6db54346fbb8990be
Michael Humbert <mike5603@comcast.net>
2016-03-24 11:19:05 -0400

    added ion pair lifetime calculation

--- rubicon/analysis/lammps/runall.py
+++ rubicon/analysis/lammps/runall.py
@@ -1,15 +1,16 @@
-# coding: utf-8
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 12 14:14:34 2015
 
-__author__ = "mhumbert"
-
-from __future__ import division, print_function, unicode_literals, \
-    absolute_import
-
+@author: mhumbert
+"""
 '''
 
         Sample driver file to calculate the MSD and diffusivity for all 
         molecules in a system as well as the center of mass radial distribution
-        function for all pairs of molecules in the system
+        function for all pairs of molecules in the system. Will also integrate
+        the rdf to get the coordination number and calculate the ion pair lifetime 
+        for the system
         
         Requires the following comments in the lammps data file starting 
         at the third line
@@ -26,8 +27,6 @@ from __future__ import division, print_function, unicode_literals, \
 
 '''
 
-from __future__ import absolute_import
-
 if __name__ == '__main__':
     from rubicon.analysis.lammps.MSD import MSD
     from rubicon.analysis.lammps.calcCOM import calcCOM
@@ -36,9 +35,9 @@ if __name__ == '__main__':
     from rubicon.analysis.lammps.COMradialnofort import COMradialdistribution
     from rubicon.analysis.lammps.getatomcharges import getatomcharges
     from rubicon.analysis.lammps.calcNEconductivity import calcNEconductivity
-    from rubicon.analysis.lammps.getcoordinationnumber import \
-        getcoordinationnumber
-
+    from rubicon.analysis.lammps.getcoordinationnumber import getcoordinationnumber
+    from rubicon.analysis.lammps.ionpair import ionpair
+    
     c = calcCOM()
     m = MSD()
     gt = gettimedata()
@@ -47,10 +46,11 @@ if __name__ == '__main__':
     gc = getatomcharges()
     ne = calcNEconductivity()
     cn = getcoordinationnumber()
+    ip = ionpair()
 
-    trjfile = 'sample_files/NaSCN.lammpstrj'
-    datfile = 'sample_files/data.water_1NaSCN'
-    logfile = 'sample_files/mol.log'
+    trjfile = 'rubicon/analysis/lammps/sample_files/NaSCN.lammpstrj'
+    datfile = 'rubicon/analysis/lammps/sample_files/data.water_1NaSCN'
+    logfile = 'rubicon/analysis/lammps/sample_files/mol.log'
     output = {}
     output['RDF'] = {}
     output['RDF']['units'] = 'unitless and angstroms'
@@ -74,12 +74,17 @@ if __name__ == '__main__':
                                    moltypel, T)
     # print(output)
     # print('Conductivity Finshed')
+    
+    ip.runionpair(comx, comy, comz, Lx, Ly, Lz, moltypel, moltype, tsjump,
+                  dt, output, skipframes = 0)
+
     output = crd.runradial(datfile, comx, comy, comz, Lx, Ly, Lz, Lx2, Ly2,
                            Lz2, output, nummoltype, moltypel, moltype,
                            firststep=1)
+    
 
-
-    # output = cn.calccoordinationnumber(output,nummoltype,moltypel,Lx*Ly*Lz)
+    output = cn.calccoordinationnumber(output,nummoltype,moltypel,Lx*Ly*Lz)
+    
     # outputfile=open('test.json', 'w')
     # json.dump(output,outputfile,indent=4)
     # outputfile.close()
