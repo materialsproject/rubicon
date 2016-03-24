cf40961192760a6c33a600ee6ee545232bb1cb55
Kiran Mathew <km468@cornell.edu>
2016-03-24 10:51:37 -0700

    some cleanup
    replace open with io.open in packmol.py
    
    fix some encoding issues

--- rubicon/analysis/lammps/runall.py
+++ rubicon/analysis/lammps/runall.py
@@ -1,31 +1,30 @@
-# -*- coding: utf-8 -*-
+# coding: utf-8
+
+from __future__ import division, print_function, unicode_literals, \
+    absolute_import
+
 """
-Created on Thu Mar 12 14:14:34 2015
-
-@author: mhumbert
+Sample driver file to calculate the MSD and diffusivity for all
+molecules in a system as well as the center of mass radial distribution
+function for all pairs of molecules in the system. Will also integrate
+the rdf to get the coordination number and calculate the ion pair lifetime
+for the system
+        
+Requires the following comments in the lammps data file starting
+at the third line
+            
+# 'number' 'molecule' molecules
+            
+where 'number' is the number of that molecule type and
+'molecule' is a name for that molecule
+            
+Do not include blank lines in between the molecule types
+        
+Outputs are stored in a dictionary called output to later be stored
+in JSON format
 """
-'''
 
-        Sample driver file to calculate the MSD and diffusivity for all 
-        molecules in a system as well as the center of mass radial distribution
-        function for all pairs of molecules in the system. Will also integrate
-        the rdf to get the coordination number and calculate the ion pair lifetime 
-        for the system
-        
-        Requires the following comments in the lammps data file starting 
-        at the third line
-            
-        # "number" "molecule" molecules
-            
-        where "number" is the number of that molecule type and
-        "molecule" is a name for that molecule
-            
-        Do not include blank lines in between the molecule types
-        
-        Outputs are stored in a dictionary called output to later be stored
-        in JSON format
-
-'''
+__author__ = "mhumbert"
 
 if __name__ == '__main__':
     from rubicon.analysis.lammps.MSD import MSD
@@ -66,25 +65,16 @@ if __name__ == '__main__':
     molcharge = gc.molchargedict(molcharges, moltypel, moltype)
     (comx, comy, comz, Lx, Ly, Lz, Lx2, Ly2, Lz2) = c.calcCOM([trjfile],
                                                               datfile)
-
     output = m.runMSD(comx, comy, comz, Lx, Ly, Lz, Lx2, Ly2, Lz2, moltype,
                       moltypel, dt, tsjump, output)
-
     output = ne.calcNEconductivity(output, molcharge, Lx, Ly, Lz, nummoltype,
                                    moltypel, T)
-    # print(output)
-    # print('Conductivity Finshed')
-    
     ip.runionpair(comx, comy, comz, Lx, Ly, Lz, moltypel, moltype, tsjump,
                   dt, output, skipframes = 0)
-
     output = crd.runradial(datfile, comx, comy, comz, Lx, Ly, Lz, Lx2, Ly2,
                            Lz2, output, nummoltype, moltypel, moltype,
                            firststep=1)
-    
-
     output = cn.calccoordinationnumber(output,nummoltype,moltypel,Lx*Ly*Lz)
-    
     # outputfile=open('test.json', 'w')
     # json.dump(output,outputfile,indent=4)
     # outputfile.close()
