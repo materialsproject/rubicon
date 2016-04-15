fba468d266be4fb9cf261078abe750dc05ec71b9
Kiran Mathew <km468@cornell.edu>
2016-04-15 13:42:31 -0700

    update io.lammps.input and more documentation

--- rubicon/io/lammps/input.py
+++ rubicon/io/lammps/input.py
@@ -16,6 +16,8 @@ from collections import OrderedDict
 
 from monty.json import MSONable
 
+from rubicon.io.lammps.data import LammpsData, LammpsForceFieldData
+
 
 __author__ = 'Kiran Mathew, Navnidhi Rajput'
 __email__ = "kmathew@lbl.gov"
@@ -39,8 +41,8 @@ class DictLammpsInput(MSONable):
             or bond type.
     """
 
-    def __init__(self, name, config_dict, lammps_data=None, data_filename=None,
-                 user_lammps_settings=None):
+    def __init__(self, name, config_dict, lammps_data=None, data_filename="in.data",
+                 user_lammps_settings={}):
         self.name = name
         self.lines = []
         self.config_dict = config_dict
@@ -48,14 +50,14 @@ class DictLammpsInput(MSONable):
         self.data_filename = data_filename
         self.config_dict["read_data"] = data_filename
         self.user_lammps_settings = user_lammps_settings
-        if user_lammps_settings:
-            self.config_dict.update(user_lammps_settings)
+        if self.user_lammps_settings:
+            self.config_dict.update(self.user_lammps_settings)
 
     @staticmethod
     def get_lines_from_dict(in_dict):
         """
         recursively convert the nested dict to an appropriate string
-        representation to be used as lammps input file.
+        representation to be used as lammps input file with the control paramters.
         """
         lines = " "
         for k1, v1 in in_dict.items():
@@ -69,43 +71,56 @@ class DictLammpsInput(MSONable):
         return lines
 
     def __str__(self):
+        """
+        string representation of the lammps input file with the control parameters
+        """
         return self.get_lines_from_dict(self.config_dict)
 
-    def write_input(self, filename=None, data_filename=None):
+    def write_input(self, filename):
         """
-        Write the main input file and the data file
+        get the string representation of the main input file and write it.
+        Also writes the data file if the lammps_data attribute is set.
+
+        Args:
+            filename (string): name of the input file
         """
         # write the main input file
         with open(filename, 'w') as f:
             f.write(self.__str__())
         # write the data file if present
         if self.lammps_data:
-            if data_filename:
-                self.lammps_data.write_data_file(filename=data_filename)
-            elif self.data_filename:
-                self.lammps_data.write_data_file(filename=self.data_filename)
-            else:
-                dfilename = "lammps_data"
-                print("No data filename provided. Writing the data to {"
-                      "}".format(dfilename))
-                self.lammps_data.write_data_file(filename=dfilename)
+            print("Writing the data to {}".format(self.data_filename))
+            self.lammps_data.write_data_file(filename=self.data_filename)
 
     @staticmethod
-    def from_file(name, filename, **kwargs):
+    def from_file(name, filename, data_filename=None, is_forcefield=False, **kwargs):
         """
-        Read in the input settings from json file as ordereddict.
-        Reads only the main input file from the json file, skips the data file.
+        Read in the input settings from json file as ordereddict. Also reads in the
+        datafile if provided.
         Note: with monty.serialization.loadfn the order of paramters in the
         json file is not preserved
+
+        Args:
+            filename (string): name of the file with the lamps control paramters
+            data_filename (string): name of the data file name
+            is_forcefield (bool): whether the data file has forcefield and topology info
+                in it
+
+        Returns:
+            DictLammpsInput
         """
         with open(filename) as f:
-            return DictLammpsInput(name,
-                                   json.load(f,
-                                                object_pairs_hook=OrderedDict),
-                                   **kwargs)
+            config_dict = json.load(f, object_pairs_hook=OrderedDict)
+        lammps_data = None
+        if data_filename:
+            if is_forcefield:
+                lammps_data = LammpsForceFieldData.from_file(data_filename)
+            else:
+                lammps_data = LammpsData.from_file(data_filename)
+        return DictLammpsInput(name, config_dict, lammps_data, data_filename, **kwargs)
 
     @property
-    def to_dict(self):
+    def as_dict(self):
         return {"@module": self.__class__.__module__,
                 "@class": self.__class__.__name__,
                 "name": self.name,
